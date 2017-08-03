eff1dbdc3fef917fb7badd93a7f922ad130744e7
showeimer <showeimer@gmail.com>
2017-08-03 16:02:30 -0400

    code formatting fix

--- main.js
+++ main.js
@@ -7,26 +7,28 @@ let output;
 
 submit.addEventListener('click', function search() {
 
-searchResults.innerHTML = "";
+  searchResults.innerHTML = "";
 
-// User's search value is concatenated by + to be able to be put into the api search
-let userSearch = input.value;
-userSearch = userSearch.split(" ").join("+");
+  // User's search value is concatenated by + to be able to be put into the api search
+  let userSearch = input.value;
+  userSearch = userSearch.split(" ").join("+");
 
-fetch(`https://itunes.apple.com/search?term=${userSearch}`)
-  .then(function(response) {
-    console.log(response);
-    response.json().then(function(data) {
-        console.log(data);
-        output = data.results;
-        console.log(output);
-        console.log(output[0]);
-      })
+  fetch(`https://itunes.apple.com/search?term=${userSearch}`)
+    .then(function(response) {
+      console.log(response);
+      response.json()
 
-      .then(function() {
-        for (let i = 0; i < output.length; i++) {
-          // Generating album covers, song and artist names, making album the clickable play button for song preview
-          searchResults.innerHTML += `
+        .then(function(data) {
+          console.log(data);
+          output = data.results;
+          console.log(output);
+          console.log(output[0]);
+        })
+
+        .then(function() {
+          for (let i = 0; i < output.length; i++) {
+            // Generating album covers, song and artist names, making album the clickable play button for song preview
+            searchResults.innerHTML += `
                 <div class="result">
                   <button id='playSong${[i]}' class="playSong" title="Play Song"></button>
                   <style>#playSong${[i]} {background-image: url('${output[i].artworkUrl100}'); width: 100px; height: 100px;}</style>
@@ -34,17 +36,17 @@ fetch(`https://itunes.apple.com/search?term=${userSearch}`)
                   <h5>${output[i].artistName}</h5>
                 </div>
                 `
-        }
-        // Event listener for play button to send clip to html audio
-        let playSong = document.querySelectorAll('.playSong');
-        for (let i = 0; i < output.length; i++) {
-          playSong[i].addEventListener('click', function() {
-            musicPlayer.innerHTML = `
+          }
+          // Event listener for play button to send clip to html audio
+          let playSong = document.querySelectorAll('.playSong');
+          for (let i = 0; i < output.length; i++) {
+            playSong[i].addEventListener('click', function() {
+              musicPlayer.innerHTML = `
                   <p>Now playing:  ${output[i].trackName} - ${output[i].artistName}</p>
                   <audio src="${output[i].previewUrl}" autoplay controls></audio>
                 `
-          })
-        }
-      })
-  })
+            })
+          }
+        })
+    })
 })
